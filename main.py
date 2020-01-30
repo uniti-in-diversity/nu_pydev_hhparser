02c9924fe94febe997b7a38d67caf635440dd427
maxim <dekin_m@mail.ru>
2020-01-30 23:05:54 +0300

    добавил логи

--- main.py
+++ main.py
@@ -4,12 +4,28 @@ web = Flask(__name__)
 
 @web.route('/')
 def index():
-    return render_template('index.html')
+    context = {
+    'title':'HeadHanter Парсер',
+    'button_name':'Парсить вакансии'
+    }
+    return render_template('index.html', **context)
+
+@web.route('/form/')
+def form():
+    return render_template('form.html')
+
 
 
 @web.route('/contacts/')
 def contacts():
-    return render_template('contacts.html')
+    context = {
+        'name': 'Декин Максим',
+        'email': "maxim.dekin@gmail.com",
+        'phone': +79257777777,
+        'telegram':'@unitiindiversity',
+        'about':'DevOps инженер, python developer'
+    }
+    return render_template('contacts.html', **context)
 
 if __name__ == "__main__":
     web.run(debug=True)
